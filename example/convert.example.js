55106c6e20aae409f5c5d5db7e778afece603110
bagaking <zfaustk@qq.com>
2019-02-11 21:54:43 +0800

    test: update examples

--- example/convert.example.js
+++ example/convert.example.js
@@ -6,4 +6,14 @@ const {
 let ret = readAndTranslate(`${__dirname}/nft.building.xlsx`, {
   plugins: [ Plugins.convert ]
 })
-console.log(JSON.stringify(ret, null, 2))
+console.log(JSON.stringify(ret.convert, null, 2))
+
+ret = readAndTranslate(`${__dirname}/token.point.xlsx`, {
+  plugins: [ Plugins.convert ]
+})
+console.log(JSON.stringify(ret.convert, null, 2))
+
+ret = readAndTranslate(`${__dirname}/token.res.xlsx`, {
+  plugins: [ Plugins.convert ]
+})
+console.log(JSON.stringify(ret.convert, null, 2))

ecad8fd575ad19f3dfd248da4a7beca26a7ca1e9
bagaking <zfaustk@qq.com>
2019-03-06 11:02:58 +0800

    fix: format of jsSerializer

--- src/serializer/jsSerializer.js
+++ src/serializer/jsSerializer.js
@@ -2,7 +2,7 @@ const Plugins = require('../plugin')
 
 const jsSerializer = {
   plugins: [Plugins.convert],
-  file: data => `${module.exports = JSON.stringify(data.convert, null, 2)}`
+  file: data => `module.exports = ${JSON.stringify(data.convert, null, 2)}`
 }
 
 module.exports = {

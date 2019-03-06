7339dd4d7db76649589429af2789228f05b8d39d
bagaking <zfaustk@qq.com>
2019-03-06 10:27:44 +0800

    feat: add exec.js

---
+++ src/exec.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+import * as Yargs from 'yargs'
+import { FileWalker } from 'kht'
+import * as Path from 'path'
+import * as Serializer from './serializer'
+import { makeCamelName } from './utils/names'
+
+const packageObj = require('../package.json')
+const argv = Yargs
+  .usage('Usage: kh-table [-i INPUT_DIR] [-o OUTPUT_DIR] [-f FORMAT]')
+  .option('input', {
+    alias: 'i',
+    default: '.',
+    describe: 'the input directory'
+  })
+  .option('output', {
+    alias: 'o',
+    default: '.',
+    describe: 'the output directory'
+  })
+  .option('format', {
+    alias: 'f',
+    describe: 'export format',
+    choices: ['json', 'js', 'ts', 'ts-interface'],
+    default: 'json'
+  })
+  .help('h')
+  .alias('h', 'help')
+  .version(packageObj.version)
+  .argv
+
+const { input, output, format } = argv
+const oPath = (output && output.startsWith('/')) ? output : Path.resolve(__dirname, output)
+const iPath = (input && input.startsWith('/')) ? input : Path.resolve(__dirname, input)
+
+const formats = {
+  'json': { suffix: 'json', serializer: Serializer.jsonSerializer },
+  'js': { suffix: 'js', serializer: Serializer.jsSerializer },
+  'ts': { suffix: 'ts', serializer: Serializer.tsSerializer },
+  'ts-interface': { suffix: 'ts', serializer: Serializer.tsInterfaceSerializer }
+}
+const formatObj = formats[format]
+
+FileWalker.forEachSync(
+  iPath,
+  fileObj => {
+    console.log('generate : ', fileObj.path)
+    Serializer.serialize(fileObj.path, oPath, {
+      [`${makeCamelName(fileObj.parsed.name)}.${formatObj.suffix}`]: formatObj.serializer
+    })
+  }, false, file => !file.match(/\..*\.swp/) && !file.startsWith('~')
+)

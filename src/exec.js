4978a1cf26a35541a9b49ed1f1237f4cf8f4a5e2
bagaking <zfaustk@qq.com>
2019-03-12 01:40:14 +0800

    feat: update exec

--- src/exec.js
+++ src/exec.js
@@ -4,7 +4,13 @@ import * as Yargs from 'yargs'
 import { FileWalker } from 'kht'
 import * as Path from 'path'
 import * as fs from 'fs-extra'
-import * as Serializer from './serializer'
+import {
+  jsonSerializer,
+  jsSerializer,
+  tsSerializer,
+  tsInterfaceSerializer,
+  serialize
+} from './serializer'
 import { makeCamelName } from './utils/names'
 
 const packageObj = require('../package.json')
@@ -37,10 +43,10 @@ const oPath = (output && output.startsWith('/')) ? output : Path.resolve(EXECUTE
 const iPath = (input && input.startsWith('/')) ? input : Path.resolve(EXECUTE_PATH, input)
 
 const formats = {
-  'json': { suffix: 'json', serializer: Serializer.jsonSerializer },
-  'js': { suffix: 'js', serializer: Serializer.jsSerializer },
-  'ts': { suffix: 'ts', serializer: Serializer.tsSerializer },
-  'ts-interface': { suffix: 'ts', serializer: Serializer.tsInterfaceSerializer }
+  'json': { suffix: 'json', serializer: jsonSerializer },
+  'js': { suffix: 'js', serializer: jsSerializer },
+  'ts': { suffix: 'ts', serializer: tsSerializer },
+  'ts-interface': { suffix: 'ts', serializer: tsInterfaceSerializer }
 }
 const formatObj = formats[format]
 
@@ -50,14 +56,14 @@ if (stat.isDirectory()) {
     iPath,
     fileObj => {
       console.log('generate : ', fileObj.path)
-      Serializer.serialize(fileObj.path, oPath, {
+      serialize(fileObj.path, oPath, {
         [`${makeCamelName(fileObj.parsed.name)}.${formatObj.suffix}`]: formatObj.serializer
       })
     }, false, file => !file.match(/\..*\.swp/) && !file.startsWith('~') && file.endsWith('xlsx')
   )
 } else if (!iPath.match(/\..*\.swp/) && !iPath.startsWith('~') && iPath.endsWith('xlsx')) {
   console.log('generate : ', iPath)
-  Serializer.serialize(iPath, oPath, {
+  serialize(iPath, oPath, {
     [`${makeCamelName(Path.parse(iPath).name)}.${formatObj.suffix}`]: formatObj.serializer
   })
 } else {

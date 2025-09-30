ff7eab3505456d7e3069c8131a324841641d8621
bagaking <zfaustk@qq.com>
2025-09-30 17:13:40 +0800

    feat(example): add minirpg relic exports

--- example/game_01_minirpg/serialize.js
+++ example/game_01_minirpg/serialize.js
@@ -18,22 +18,37 @@ const tables = [
   { file: 'items.xlsx', stem: 'items' },
   { file: 'enemies.xlsx', stem: 'enemies' },
   { file: 'stages.xlsx', stem: 'stages' },
+  { file: 'relics.xlsx', stem: 'relics' },
   { file: 'global_config.xlsx', stem: 'global_config' }
+]
+
+const serializerConfigs = [
+  {
+    getFileName: stem => `${stem}.json`,
+    serializer: jsonSerializer
+  },
+  {
+    getFileName: stem => `${stem}.ts`,
+    serializer: tsSerializer
+  },
+  {
+    getFileName: stem => `${stem}Interface.ts`,
+    serializer: tsInterfaceSerializer
+  }
 ]
 
 function main() {
   fs.ensureDirSync(outDir)
   const context = loadContext(baseDir)
-  const serializerList = [jsonSerializer, tsSerializer, tsInterfaceSerializer]
+  const serializerList = serializerConfigs.map(({ serializer }) => serializer)
   serializeContext(outDir, serializerList, context)
 
   for (const { file, stem } of tables) {
     const src = Path.resolve(baseDir, file)
-    const serializerMap = {
-      [`${stem}.json`]: jsonSerializer,
-      [`${stem}.ts`]: tsSerializer,
-      [`${stem}Interface.ts`]: tsInterfaceSerializer
-    }
+    const serializerMap = serializerConfigs.reduce((acc, cfg) => {
+      acc[cfg.getFileName(stem)] = cfg.serializer
+      return acc
+    }, {})
     serialize(src, outDir, serializerMap, context)
     console.log(`[minirpg] serialized ${file}`)
   }
@@ -55,6 +70,7 @@ function writeWebDemo(targetDir) {
     ['__SKILLS_JSON__', loadJsonForScript(Path.resolve(targetDir, 'skills.json'))],
     ['__ITEMS_JSON__', loadJsonForScript(Path.resolve(targetDir, 'items.json'))],
     ['__ENEMIES_JSON__', loadJsonForScript(Path.resolve(targetDir, 'enemies.json'))],
+    ['__RELICS_JSON__', loadJsonForScript(Path.resolve(targetDir, 'relics.json'))],
     ['__STAGES_JSON__', loadJsonForScript(Path.resolve(targetDir, 'stages.json'))],
     ['__GLOBAL_JSON__', loadJsonForScript(Path.resolve(targetDir, 'global_config.json'))]
   ])
